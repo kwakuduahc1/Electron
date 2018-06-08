1e5221c515ed1571f962c6a1fcdf7d133f0e2b72
Duah Kwaku <ibrahimkwakuduah@hotmail.com>
2018-06-08 08:24:14 +0000

    Set up application context

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Electron.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=assistant.db;", x =>
+            {
+                x.SuppressForeignKeyEnforcement(false);
+                x.UseRelationalNulls(true);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.Entity<Locations>(x => x.HasData(
+            //    new Locations { Location = "Kumasi", LocationsID = 1, Region = "Ashanti" },
+            //    new Locations { Location = "Accra", LocationsID = 2, Region = "Greater Accra" },
+            //    new Locations { Location = "Nalerigu", Region = "Northern", LocationsID = 3 }
+            //    ));
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.// For example, you can rename the ASP.NET Identity table names and more.// Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public virtual DbSet<Locations> Questions { get; set; }
+    }
+
+    public class Locations
+    {
+        public int LocationsID { get; set; }
+
+        public string Location { get; set; }
+
+        public string Region { get; set; }
+    }
+}
